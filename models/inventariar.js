ff66e08b619768cf0634ee6fd618bb6a3de8d303
Martin De Lojo <martinmdl42@gmail.com>
2021-04-04 15:24:08 -0300

    segundo commit

---
+++ models/inventariar.js
@@ -0,0 +1,22 @@
+/* const {Schema, model} = require('mongoose');
+
+invSchema = new Schema({
+    img: {
+        default: 'http://image.png'
+    },
+    prod: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    desc: {
+        type: String,
+        require: true
+    },
+    precio: {
+        type: Number,
+        require: true
+    }
+})
+
+module.exports = model('Producto', invSchema); */
